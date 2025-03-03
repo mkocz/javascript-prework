545ac1dfb0696317b504275045d608f5028620fe
mkocz <82184888+mkocz@users.noreply.github.com>
2025-03-03 21:21:45 +0100

    Fix spacing part2

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if (randomNumber == 1) {
   computerMove = 'kamień';
-}else if (randomNumber == 2){
+} else if (randomNumber == 2) {
   computerMove = 'papier'
-}else if (randomNumber == 3){
+} else if (randomNumber == 3) {
   computerMove = 'nożyce'
-}   
+}
 
 printMessage('Mój ruch to: ' + computerMove);
 

6cf6bb9af996c9dc5daed5ac0bcb668b9f0f8cd0
mkocz <82184888+mkocz@users.noreply.github.com>
2025-03-03 21:08:30 +0100

    Fix spacing

--- js/script.js
+++ js/script.js
@@ -20,35 +20,35 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if (playerInput == '1') {
   playerMove = 'kamień';
-}else if (randomNumber == 2){
+} else if (randomNumber == 2) {
   playerMove = 'papier'
-}else if (randomNumber == 3){
+} else if (randomNumber == 3) {
   playerMove = 'nożyce'
 }   
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
+if (computerMove == 'kamień' && playerMove == 'papier') {
   printMessage('Ty wygrywasz!');
-}else if ( computerMove == 'kamień' && playerMove == 'nożyce'){
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
   printMessage('Wygrywa komputer!');
-}else if ( computerMove == 'kamień' && playerMove == 'kamień'){
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
   printMessage('Remis!');
-}else if ( computerMove == 'papier' && playerMove == 'papier'){
+} else if (computerMove == 'papier' && playerMove == 'papier') {
   printMessage('Remis!');
-}else if ( computerMove == 'papier' && playerMove == 'kamień'){
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
   printMessage('Wygrywa komputer!');
-}else if ( computerMove == 'papier' && playerMove == 'nożyce'){
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
   printMessage('Ty wygrywasz!');
-}else if ( computerMove == 'nożyce' && playerMove == 'nożyce'){
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
   printMessage('Remis!')
-}else if ( computerMove == 'nożyce' && playerMove == 'papier'){
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
   printMessage('Wygrywa komputer!')
-}else if ( computerMove == 'nożyce' && playerMove == 'kamień'){
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
   printMessage('Ty wygrywasz!')
-}else if (playerMove == 'nieznany ruch'){
+} else if (playerMove == 'nieznany ruch') {
   printMessage('Nieprawidłowa wartość!')
 }
   

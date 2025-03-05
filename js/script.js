4971f08dda3e181aad75a3cf6c0a9e9172f3d31e
Maria Koczwara <mkoczwara@poczta.onet.pl>
2025-03-05 13:36:40 +0100

    Add button controls

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+function playGame( argPlayerInput ) {
+	clearMessages();
+	
+	let playerInput = argPlayerInput;
+	
+	let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+	console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = getMoveName(randomNumber);
+	let computerMove = getMoveName(randomNumber);
 
-printMessage('Mój ruch to: ' + computerMove);
+	printMessage('Mój ruch to: ' + computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+	console.log('Gracz wpisał: ' + playerInput);
 
-console.log('Gracz wpisał: ' + playerInput);
+	let playerMove = getMoveName(playerInput);
 
-let playerMove = getMoveName(playerInput);
+	printMessage('Twój ruch to: ' + playerMove);
 
-printMessage('Twój ruch to: ' + playerMove);
+	displayResult(computerMove, playerMove);
+};
 
-displayResult(computerMove, playerMove);
-  
+document.getElementById('play-rock').addEventListener('click', function() {
+	playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function() {
+	playGame(2); 
+});
+document.getElementById('play-scissors').addEventListener('click', function() {
+	playGame(3);
+});
